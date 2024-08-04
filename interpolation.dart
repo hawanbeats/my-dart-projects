cd69915849912a8805a99ff28925f929ab2d4835
hawanbeats <hawanbeats@gmail.com>
2024-08-04 22:44:18 +0300

    change

---
+++ interpolation.dart
@@ -0,0 +1,11 @@
+void main(){
+  var isim = "hasan";
+  var soyIsim = "damirli";
+  print("$isim $soyIsim"); //interpolation
+  print("$soyIsim'de bulunan karakter sayisi:" + soyIsim.length.toString());
+  print("Karakter sayisi: ${soyIsim.length}");
+
+  int en = 12;
+  int boy = 10;
+  print("Eni $en boyu $boy olan dikdortkenin alani: ${en*boy}"Eni $en boyu $boy olan dikdortkenin alani: ${en*boy}Eni $en boyu $boy olan dikdortkenin alani: ${en*boy});
+}
