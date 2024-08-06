e16fbc90e0b6d49f29a983c70340884de1f40684
hawanbeats <hawanbeats@gmail.com>
2024-08-06 17:33:42 +0300

    wow

---
+++ bolum2/switch_case.dart
@@ -0,0 +1,32 @@
+void main(){
+  var not = "BB";
+  switch(not){
+    case "AA":
+    print("Notunuz 90-100 araligindadir");
+    break;
+
+    case "BA":
+    print("Notunuz 80-90 araligindadir");
+    break;
+
+    case "BB":
+    print("Notunuz 70-80 araligindadir");
+    break;
+
+    case "CB":
+    print("Notunuz 60-70 araligindadir");
+    break;
+
+    case "CC":
+    print("Notunuz 50-60 araligindadir");
+    break;
+
+    case "FF":
+    print("Notunuz cok dusuk");
+    break;
+
+    default:{
+      print("Hatali veya yanlis deger");
+    }
+  }
+}
