36efdad08805a61c5bb1d7ab092ca1908610d300
hawanbeats <hawanbeats@gmail.com>
2024-08-07 15:20:31 +0300

    hhh

--- bolum2/fonksiyonlar_ornek2.dart
+++ bolum2/fonksiyonlar_ornek2.dart
@@ -1,1 +1,11 @@
-//
+//soru 2
+//daire alanini hesaplayan fonksiyon yaz. pi sayisi opsiyonel olmali. eger verilmediyse varsayilan olarak 3.14 olarakm hesaplanmali.
+
+void main(){
+  double alan = daireAlanHesapla(5,pi: 3);
+  print("dairenin alani = $alan"); 
+}
+
+double daireAlanHesapla(double r,{double pi = 3.14}){
+  return pi * r * r;
+}
