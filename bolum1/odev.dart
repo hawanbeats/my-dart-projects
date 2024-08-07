d3ab5e90f841d39438299c595330b39257092f6f
hawanbeats <hawanbeats@gmail.com>
2024-08-07 15:44:43 +0300

    reee

--- bolum1/odev.dart
+++
@@ -1,7 +0,0 @@
-void main(){
-  int birinciKenar = 3;
-  int ikinciKenar = 4;
-  int ucuncuKenar = 5;
-  print("birinci kenari $birinciKenar, ikinci kenari $ikinciKenar, ucuncu kenari $ucuncuKenar olan ucgenin cevresi ${birinciKenar+ikinciKenar
-  +ucuncuKenar}'dir");
-}
