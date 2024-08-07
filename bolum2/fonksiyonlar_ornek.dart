9346b0b743bc1484809ed0b53b7a0250246e24d7
hawanbeats <hawanbeats@gmail.com>
2024-08-07 14:59:19 +0300

    gggg

--- bolum2/fonksiyonlar_ornek.dart
+++
@@ -1,7 +0,0 @@
-void main(){
-  //soru 1
-  //parametre olarak bir tane int sayi alan ve aldigi degere kadar olan cift sayilarin toplamini ver bir fonksiyon yaz
-
-}
-
-int 
---
+++ bolum2/fonksiyonlar_ornek1.dart
@@ -0,0 +1,17 @@
+void main(){
+  //soru 1
+  //parametre olarak bir tane int sayi alan ve aldigi degere kadar olan cift sayilarin toplamini ver bir fonksiyon yaz
+  int toplam = ciftSayiToplam(8); 
+  print("toplam = $toplam");
+}
+
+int ciftSayiToplam(int sayi){
+  int toplam = 0;
+  for(int i = 0;i <= sayi;i++){
+    if(i % 2 == 0){
+      toplam += i;
+      print(i);
+    }
+  }
+  return toplam;
+}
