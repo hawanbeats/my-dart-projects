d730a751569e05933943061416a70cfb14353a32
hawanbeats <hawanbeats@gmail.com>
2024-08-07 15:43:59 +0300

    gggg

--- bolum2/fonksiyonlar_ornek3.dart
+++ bolum2/fonksiyonlar_ornek3.dart
@@ -1,2 +1,18 @@
 //soru 3
 //bir ucgenin kenarlarini isimlendirilmis parametre olarak alan fonksiyon yaz. bu fonksiyon kenar degerlerine gore ucgenin tipini ekrana yazdirsin.
+
+void main(){
+  ucgenTipi(sayi1: 2,sayi2: 4,sayi3: 2);
+}
+
+void ucgenTipi({int sayi1 = 1, int sayi2 = 1,int sayi3 = 1}){
+  if((sayi1 == sayi2) && (sayi1 == sayi3)){
+    print("ikizkenar ucgen");
+  }
+  else if((sayi1 != sayi2) && (sayi1 != sayi3)){
+    print("cesitkenar ucgen");
+  }
+  else{
+    print("eskenar ucgen");
+  } 
+}
