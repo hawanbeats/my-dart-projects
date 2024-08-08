90772596fc7aa96febdc370a380d09ba0ef202fa
hawanbeats <hawanbeats@gmail.com>
2024-08-08 22:21:07 +0300

    hfghgf

--- bolum3/setler.dart
+++ bolum3/setler.dart
@@ -1,5 +1,5 @@
 void main(){
-  Set<String> isimler = Set();
+  Set<String> isimler = Set(); //bos set olusturma
   isimler.add("hasan");
   isimler.add("hasan");
   isimler.add("ali");
@@ -16,14 +16,14 @@ void main(){
   print(sonuc);
   print(isimler);
 
-  Set<int> numaralar = Set.from([1,4,5,6,2,1,6,9]);
+  Set<int> numaralar = Set.from([1,4,5,6,2,1,6,9]); //ici dolu set olusturma
   List<int> ciftsayilar = [0,2,4,6,8,10];
   for(int numara in numaralar){
     print("no: $numara");
   }
-  numaralar.addAll(ciftsayilar);
+  numaralar.addAll(ciftsayilar); //sete listeyi ekleme
   print(numaralar);
-  numaralar.clear();
+  numaralar.clear(); //seti temizleme
   numaralar.add(0);
   print(numaralar);
 }
