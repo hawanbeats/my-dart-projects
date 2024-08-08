cf64d60902169fc68a7c4fd455386b7b1c6c12d6
hawanbeats <hawanbeats@gmail.com>
2024-08-08 21:38:35 +0300

    tetwtw

--- bolum3/setler.dart
+++ bolum3/setler.dart
@@ -12,5 +12,18 @@ void main(){
     print("isimler: $isim");
   }
 
-  
+  bool sonuc = isimler.remove("hasan");
+  print(sonuc);
+  print(isimler);
+
+  Set<int> numaralar = Set.from([1,4,5,6,2,1,6,9]);
+  List<int> ciftsayilar = [0,2,4,6,8,10];
+  for(int numara in numaralar){
+    print("no: $numara");
+  }
+  numaralar.addAll(ciftsayilar);
+  print(numaralar);
+  numaralar.clear();
+  numaralar.add(0);
+  print(numaralar);
 }
