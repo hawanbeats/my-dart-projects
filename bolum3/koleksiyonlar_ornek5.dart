c3215f3c66b68798a3b64c68c2e07b36f6c8d877
hawanbeats <hawanbeats@gmail.com>
2024-08-09 19:46:47 +0300

    grgreger

--- bolum3/koleksiyonlar_ornek5.dart
+++ bolum3/koleksiyonlar_ornek5.dart
@@ -1,6 +1,21 @@
+import 'dart:io';
+
 void main(){
 //soru 5
-//kullanicidan aldiginiz integer pozitif sayilari bir listede tut ve kullanici 0 girdiginde girilen sayilarin ortalamasini ekrana yazdir.
+//kullanicidan aldiginiz integer pozitif sayilari bir listede tut ve kullanici -1 girdiginde girilen sayilarin ortalamasini ekrana yazdir.
 
+  int not;
+  var list = <int>[];
   
+  do{
+    print("lutfen deger giriniz:");
+    not = int.parse(stdin.readLineSync()!);
+    if(not != -1){
+      list.add(not);
+    }
+  }
+  while(not != -1);
+
+  double ortnot = list.reduce((a, b) => a + b) / list.length;
+  print(ortnot);
 }
