22f22a98ffd294f0078016cc1e6ac0c903a840fa
hawanbeats <hawanbeats@gmail.com>
2024-08-09 18:28:54 +0300

    jhtj

--- bolum3/koleksiyonlar_ornek4.dart
+++ bolum3/koleksiyonlar_ornek4.dart
@@ -1,4 +1,6 @@
 void main(){
 //soru 4
-//5 elemanli iki farkli liste olustur. elemanlar 0-50ye rastgele sekilde olustur ve elemanlari tek bir listeye aktar. olusan son listenin elemanlarinin karelerini tutan set yapisi olusturup ekrana yazdir.
+//5 elemanli iki farkli liste olustur. elemanlar 0-50ye rastgele sekilde olusturulsun ve elemanlari tek bir listeye aktar. olusan son listenin elemanlarinin karelerini tutan set yapisi olusturup ekrana yazdir.
+
+
 }
