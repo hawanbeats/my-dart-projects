b3311c2e7ae1614b3dd626911b33e2d8eb011637
hawanbeats <hawanbeats@gmail.com>
2024-08-09 21:10:35 +0300

    sdfdsfs

---
+++ bolum4/class.dart
@@ -0,0 +1,17 @@
+void main(){
+  Ogrenci hasan = Ogrenci();
+  Ogrenci ali = Ogrenci();
+  hasan.ogrenciNo = 05;
+  ali.ogrenciSoyad = "damirli";
+  hasan.aktifMi = true;
+}
+
+class Ogrenci{
+  int ogrenciNo = 1;
+  String ogrenciSoyad = "";
+  bool aktifMi = true;
+}
+
+void dersCalis(){
+  print("ogrenci ders calisiyor");
+}
