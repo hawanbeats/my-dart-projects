733a70eb1b1d62d16efecc39e54d1524596da0e2
hawanbeats <hawanbeats@gmail.com>
2024-08-09 18:27:48 +0300

    jgjhg

--- bolum3/koleksiyonlar_ornek1.dart
+++ bolum3/koleksiyonlar_ornek1.dart
@@ -1,6 +1,6 @@
 void main(){
 //soru 1
-//sehirleri tutan liste olustur ve 4 tane il ekleyip sirasiyla ekrana yazdir
+//sehirleri tutan liste olustur ve 4 tane il ekleyip sirasiyla ekrana yazdir.
   var sehirler = <String>["istanbul","ankara","izmir","bursa"];
   print(sehirler);
 
---
+++ bolum3/koleksiyonlar_ornek4.dart
@@ -0,0 +1,4 @@
+void main(){
+//soru 4
+//5 elemanli iki farkli liste olustur. elemanlar 0-50ye rastgele sekilde olustur ve elemanlari tek bir listeye aktar. olusan son listenin elemanlarinin karelerini tutan set yapisi olusturup ekrana yazdir.
+}
