61ff935efb4f97ba5ac11033562b741aad29e502
hawanbeats <hawanbeats@gmail.com>
2024-08-10 14:28:03 +0300

    hfhgf

---
+++ bolum4/ornek sorular/class_ornek1.dart
@@ -0,0 +1,10 @@
+void main(){
+//soru 1
+//CemberDaire  isimli sinif olustur ve icerisinde yaricap alan kurucusu olsun. ayrica cevre ve alanini hesaplayan metotlar olsun (pi sayisini 3.14 al)
+
+
+}
+
+class CemberDaire{
+
+}
