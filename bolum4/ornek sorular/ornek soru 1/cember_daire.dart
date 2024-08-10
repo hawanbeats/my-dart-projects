259627c29f911f6d7dc63f0d0d4a79557f656e4c
hawanbeats <hawanbeats@gmail.com>
2024-08-10 14:37:21 +0300

    fhfghhfg

--- bolum4/ornek sorular/ornek soru 1/cember_daire.dart
+++ bolum4/ornek sorular/ornek soru 1/cember_daire.dart
@@ -2,16 +2,24 @@ class CemberDaire{
   int _yariCap = 1;
   double _pi = 3.14;
 
-  CemberDaire(int yariCap){
-    yariCapKontrol = yariCap;
+  CemberDaire(int _yariCap){
+    _yariCapKontrol = _yariCap;
   }
 
-  void set yariCapKontrol(int deger){
+  void set _yariCapKontrol(int deger){
     if(deger > 0){
       _yariCap = deger;
     }
     else{
       _yariCap = 1;
     }
+  }
+
+  double cevreHesapla(){
+    return 2 * _pi * _yariCap; 
+  }
+
+  double alanHesapla(){
+    return _pi * _yariCap * _yariCap;
   }
 }
--- bolum4/ornek sorular/ornek soru 1/class_ornek1.dart
+++ bolum4/ornek sorular/ornek soru 1/class_ornek1.dart
@@ -3,6 +3,7 @@ import 'cember_daire.dart';
 void main(){
 //soru 1
 //CemberDaire  isimli sinif olustur ve icerisinde yaricap alan kurucusu olsun. ayrica cevre ve alanini hesaplayan metotlar olsun (pi sayisini 3.14 al)
-  CemberDaire c1 = CemberDaire(-5);
-
+  CemberDaire c1 = CemberDaire(2);
+  print("cevre: ${c1.cevreHesapla()}");
+  print("alan: ${c1.alanHesapla()}");
 }
