44d97953f5a8fbea6e8e1abfa463d321ee405e6e
hawanbeats <hawanbeats@gmail.com>
2024-08-11 21:18:18 +0300

    gddfgdf

--- bolum5/final_const.dart
+++ bolum5/final_const.dart
@@ -2,10 +2,27 @@ void main(){
   final list1 = [1, 2, 3];
   final list2 = [1, 2, 3];
 
+  const list3 = [4, 5, 6];
+  const list4 = [4, 5, 6];
+
+  list1.add(4);
+  print(list1);
+  
+  /*
+  list3.add(5); constla tanimlanan listeye ekleme-cikarma yapilamaz
+  */
+
   if(list1 == list2){
-    print("esit");
+    print("finalle tanimlanan listeler esit");
   }
   else{
-    print("esit degil");
+    print("finalle tanimlanan listeler esit degil");
+  }
+
+  if(list3 == list4){
+    print("constla tanimlanan listeler esit");
+  }
+  else{
+    print("constla tanimlanan listeler esit degil");
   }
 }
