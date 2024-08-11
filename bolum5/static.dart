30348e9d490aa328c9a4f7cf5561824883177841
hawanbeats <hawanbeats@gmail.com>
2024-08-11 20:38:38 +0300

    hfghfgfg

--- bolum5/static.dart
+++ bolum5/static.dart
@@ -1,13 +1,21 @@
 void main(){
   Matematik toplam = Matematik(50, 20);
   toplam.topla();
+  toplam.topla();
+  toplam.topla();
+
+  Matematik fark = Matematik(40, 5);
+  fark.cikar();
+  fark.cikar();
 
   print(Matematik.pi);
+  print(Matematik.toplamIslemSayisi);
 }
 
 class Matematik{
   int sayi1 = 0;
   int sayi2 = 0;
+  static int toplamIslemSayisi = 0;
 
   static double pi = 3.14;
 
@@ -18,10 +26,12 @@ class Matematik{
   Matematik(this.sayi1, this.sayi2);
 
   void topla(){
+    toplamIslemSayisi++;
     print("toplam: ${sayi1 + sayi2}");
   }
 
   void cikar(){
+    toplamIslemSayisi++;
     print("fark: ${sayi1 - sayi2}");
   }
 }
