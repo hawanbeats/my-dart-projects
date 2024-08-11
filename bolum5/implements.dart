f67005abbdb72d39d03d699b9da8b846823b34e5
hawanbeats <hawanbeats@gmail.com>
2024-08-11 18:05:40 +0300

    fsfsdfd

---
+++ bolum5/implements.dart
@@ -0,0 +1,45 @@
+void main(){
+
+}
+
+abstract class Hayvan{
+
+}
+
+abstract class Ucabilen{
+  void ucan();
+}
+
+abstract class Kosabilen{
+  void kosan();
+}
+
+abstract class Havlayabilen{
+  void havlayan();
+}
+
+class Kopek extends Hayvan implements Havlayabilen, Kosabilen{
+  @override
+  void havlayan() {
+    print("havliyor");
+  }
+  
+  @override
+  void kosan() {
+    print("kopek kosuyor");
+  }
+}
+
+class Kartal extends Hayvan implements Ucabilen{
+  @override
+  void ucan() {
+    print("kartal ucuyor");
+  }
+}
+
+class Insan implements Kosabilen{
+  @override
+  void kosan() {
+    print("insan kosuyor");
+  }
+}
