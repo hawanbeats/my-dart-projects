ef0a2fe401cb0a31c78efe622cd93b459bc246ea
hawanbeats <hawanbeats@gmail.com>
2024-08-11 22:16:45 +0300

    rhthrt

---
+++ bolum5/immutable.dart
@@ -0,0 +1,13 @@
+void main(){
+  Ogrenci hasan = Ogrenci("hasan", 7);
+  hasan = Ogrenci("ali", 10);
+
+  
+}
+
+class Ogrenci{
+  String isim = "";
+  int id = 0;
+
+  Ogrenci(this.isim, this.id);
+}
