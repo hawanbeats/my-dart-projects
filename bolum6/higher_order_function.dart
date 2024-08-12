0f3c67329240da94be463c1e2e4035196cf47aee
hawanbeats <hawanbeats@gmail.com>
2024-08-12 19:00:44 +0300

    jkjkhhkjkhj

--- bolum6/higher_order_function.dart
+++ bolum6/higher_order_function.dart
@@ -1,7 +1,14 @@
 void main(){
   List<int> liste = [1, 2, 3];
 
+  liste.forEach(listeicin);
+/*
   liste.forEach((element){
     print("element: $element");
   });
+*/
+}
+
+void listeicin(int deger){
+  print("deger: $deger");
 }
