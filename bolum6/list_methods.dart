4b824c27a1c380d7806f4ecd47724159d435854c
hawanbeats <hawanbeats@gmail.com>
2024-08-13 20:44:29 +0300

    ygjgjh

---
+++ bolum6/list_methods.dart
@@ -0,0 +1,31 @@
+void main(){
+  Person hasan = Person(3, "hasan");
+  Ogrenci ali = Ogrenci(1, "ali", 8);
+  var yunus = Person(6, "yunus");
+  var emre = Ogrenci(7, "emre", 4);
+
+  List<Person> tumOgrenciler = [hasan, ali, yunus, emre];
+  print(tumOgrenciler);
+}
+
+class Person{
+  int id = 0;
+  String isim = "";
+
+  Person(this.id, this.isim);
+
+  @override
+  String toString(){
+    return "id: $id ve isim: $isim";
+  }
+}
+
+class Ogrenci extends Person{
+  int dersSayi = 0;
+  Ogrenci(id, isim, dersSayi) : super(id, isim);
+
+  @override
+  String toString(){
+    return "id: $id, isim: $isim ve alinan ders sayisi: $dersSayi";
+  }
+}
