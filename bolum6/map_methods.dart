9a799af9eebeb358d61e69ea779dbd1174c1d656
hawanbeats <hawanbeats@gmail.com>
2024-08-14 13:34:57 +0300

    hghjjgh

--- bolum6/map_methods.dart
+++ bolum6/map_methods.dart
@@ -8,20 +8,20 @@ void main(){
   map['renk'] = "kirmizi";
 
   var yeniMap = Map.from({'deger': 22});
-  var mapFromEntries = Map.fromEntries(map.entries);
+  var mapFromEntries = Map.fromEntries(map.entries); //Map.fromEntries
   print(mapFromEntries);
 
   var liste = [1, 2, 3, 4];
-  var mapFromIterables = Map.fromIterable(liste);
+  var mapFromIterables = Map.fromIterable(liste); //Map.fromIterable
   print(mapFromIterables);
 
-  var mapFromIterables2 = Map<String, String>.fromIterable(liste, 
+  var mapFromIterables2 = Map<String, String>.fromIterable(liste,  //Map<String, String>.fromIterable(iterable, key: (item) => "${item * 2}");
   key: (item){
     return '$item';
   },
   value: (item) => '${item * 2}');
   print(mapFromIterables2);
 
-  var mapUpdate = map.update('id', (value) => value * 3);
+  var mapUpdate = map.update('id', (value) => value * 3); //map.uptade()
   print(mapUpdate);
 }
