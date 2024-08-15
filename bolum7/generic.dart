c2ccfcdd6f4596ea4e6f3931abb1fbc58bc45824
hawanbeats <hawanbeats@gmail.com>
2024-08-15 21:00:30 +0300

    sfdfdsfds

--- bolum7/generic.dart
+++ bolum7/generic.dart
@@ -1,12 +1,16 @@
 void main(){
-  List liste = [];
+  List<String> liste = [];
 
   liste.add("hasan");
-  liste.add(22);
-  liste.add(true);
+  liste.add("ogrenci");
 
   yazdir(liste);
 }
+
+//List<E> ==> element
+//Map<K, V> ==> key, value
+//R ==> metodlarin return tipleri
+//Ogrenci<T extends Insan> 
 
 yazdir(List list){
   print(list[0].length);
