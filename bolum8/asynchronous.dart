1fc090c0c1bf2b4846ef56a6b94fa279c7bf06ac
hawanbeats <hawanbeats@gmail.com>
2024-08-16 21:53:34 +0300

    sfdsfd

--- bolum8/asynchronous.dart
+++ bolum8/asynchronous.dart
@@ -1,8 +1,13 @@
 void main(){
   print("anne cocugu ekmek almaya yollar");
-
+  uzunSurenIslem();
+  print("masa hazirlanir");
+  print("kahvalti hazir");
 }
 
 void uzunSurenIslem(){
-  
+  print("cocuk ekmek almak icin evden cikar");
+  Future.delayed(Duration(seconds: 10), (){
+    print("cocuk ekmekle eve girer");
+  });
 }
