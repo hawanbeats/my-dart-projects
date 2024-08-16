324698120ea463df81e2aa89795d9244534881a0
hawanbeats <hawanbeats@gmail.com>
2024-08-16 22:22:19 +0300

    jhggjhjgh

--- bolum8/asynchronous.dart
+++ bolum8/asynchronous.dart
@@ -1,13 +1,16 @@
 void main(){
   print("anne cocugu ekmek almaya yollar");
-  uzunSurenIslem();
+  print("cocuk ekmek almak icin evden cikar");
+
+  Future<String> sonuc = uzunSurenIslem();
+  sonuc.then((String value) => print(value));
+
   print("masa hazirlanir");
   print("kahvalti hazir");
 }
 
-void uzunSurenIslem(){
-  print("cocuk ekmek almak icin evden cikar");
-  Future.delayed(Duration(seconds: 10), (){
-    print("cocuk ekmekle eve girer");
+Future<String> uzunSurenIslem(){
+  return Future.delayed(Duration(seconds: 5), (){
+    return "cocuk ekmekle eve girer";
   });
 }
