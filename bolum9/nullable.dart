dc15f7a2ae26b0ec3a585a27c61c873536618e7a
hawanbeats <hawanbeats@gmail.com>
2024-08-18 15:39:06 +0300

    dgffddgf

---
+++ bolum9/nullable.dart
@@ -0,0 +1,9 @@
+void main(){
+  List<String> stringListesi = ['emre', 'hasan', 'ali'];
+  List<String>? nullOlabilecekStringListesi ;
+  List<String?> nullOlabilecekStringleriTutanListe = ['emre', null, 'hasan'];
+
+  print('string listesi $stringListesi');
+  print('null olabilecek liste $nullOlabilecekStringListesi');
+  print('nullOlabilecekStringleriTutanListe $nullOlabilecekStringleriTutanListe');
+}
